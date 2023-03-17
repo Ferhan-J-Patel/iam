65a1bc8ac3c6ef535d5ea73316d5eed0c195bb5b
Chetan-Operisoft <128120269+Chetan-Operisoft@users.noreply.github.com>
2023-03-17 18:41:19 +0530

    ljlk
    ;lk;l

--- main.tf
+++ main.tf
@@ -6,11 +6,8 @@ resource "aws_iam_user" "newuser" {
     name = "chetan"
 }
 
-resource "aws_tam_policy" "ec2_policy" {
-  name = "ec2-policy"
-  policy = data.aws_iam_policy_document.ec2_policy.json
-}
-resource "aws_iam_user_policy_attachment" "chetan_user_attachment" {
-  policy_arn = aws_iam_policy.ec2_policy.arn
-  user = aws_iam_user.chetan_user.name
+resource "aws_iam_policy" "AmazonEC2FullAccess"
+  name = "AmazonEC2FullAccess"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+  user = aws_iam_user.example_user.name
 }
