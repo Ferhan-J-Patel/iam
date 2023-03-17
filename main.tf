6a0bbfac68c2197212119e20f428bf81a5d17e85
Chetan-Operisoft <128120269+Chetan-Operisoft@users.noreply.github.com>
2023-03-17 18:01:17 +0530

    jhkj
    bkjh

--- main.tf
+++ main.tf
@@ -6,3 +6,6 @@ resource "aws_iam_user" "newuser" {
     name = "chetan"
 }
 
+resource "aws_iam_role_policy_attachment" "AmazonEC2FullAccess" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"  
+}
