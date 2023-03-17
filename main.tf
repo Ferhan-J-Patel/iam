cd2264fb951820ca5bc758d437659a4c8f604b5c
Chetan-Operisoft <128120269+Chetan-Operisoft@users.noreply.github.com>
2023-03-17 17:48:41 +0530

    ljkl
    lkjl

--- main.tf
+++ main.tf
@@ -6,10 +6,3 @@ resource "aws_iam_user" "newuser" {
     name = "chetan"
 }
 
-resource "aws_iam_role_policy" "AmazonEC2FullAccess"
-name = "AmazonEC2FullAccess"
-role = "${aws_iam_role.test_role.id}"
-
-  policy = "${file("ec2-policy.json")}"
- }
-
