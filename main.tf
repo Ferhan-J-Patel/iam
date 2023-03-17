966762e794a96ed4c6a6d9d59c6c7eabd2520815
Chetan-Operisoft <128120269+Chetan-Operisoft@users.noreply.github.com>
2023-03-17 17:43:36 +0530

    hj
    hj

--- main.tf
+++ main.tf
@@ -6,4 +6,10 @@ resource "aws_iam_user" "newuser" {
     name = "chetan"
 }
 
+resource "aws_iam_role_policy" "ec2_policy"
+name = "ec2_policy"
+role = "${aws_iam_role.test_role.id}"
+
+  policy = "${file("ec2-policy.json")}"
+ }
 
