75b4c3fa627b43b66e415556d259edff75ed78c4
Chetan-Operisoft <128120269+Chetan-Operisoft@users.noreply.github.com>
2023-03-17 18:57:03 +0530

    jkhjk
    iihk

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ resource "aws_iam_user" "newuser" {
     name = "chetan"
 }
 
-resource "aws_iam_policy" "AmazonEC2FullAccess"
+resource "aws_iam_policy" "AmazonEC2FullAccess" {
   name = "AmazonEC2FullAccess"
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
-  user = aws_iam_user.example_user.name
+  user = aws_iam_user.chetan
 }
