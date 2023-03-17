a9bf76d211ba8f6b35c2f6d166f44314aa7e44bb
Chetan-Operisoft <128120269+Chetan-Operisoft@users.noreply.github.com>
2023-03-17 17:16:13 +0530

    k
    j

--- main.tf
+++ main.tf
@@ -7,18 +7,3 @@ resource "aws_iam_user" "newuser" {
 }
 
 
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": "ec2:*",
-            "Resource": "*",
-            "Effect": "Allow",
-            "Condition": {
-                "StringEquals": {
-                    "ec2:Region": "us-east-2"
-                }
-            }
-        }
-    ]
-}
