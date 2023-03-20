a87a262c110b5f5fe461e391d1a545de4bedc627
Chetan-Operisoft <128120269+Chetan-Operisoft@users.noreply.github.com>
2023-03-20 15:45:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,4 +34,13 @@ output "account_id" {
   value = aws_iam_user.newuser.unique_id
 }
 
+locals {
+    secret_access_key_file = "E:\aws/access_key.txt"
+}
+
+resource "local_file" "secret_access_key" {content  = join("", ("Access Key ID: ",output.access_key_id,"\n","Secret Access Key: ",output.secret_access_key,"\n"))
+  filename = local.secret_access_key_file
+}
+
+
 
