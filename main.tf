93c2da0e35d6378c39c1a3caf9498f56b04e739b
Chetan-Operisoft <128120269+Chetan-Operisoft@users.noreply.github.com>
2023-03-20 13:18:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,13 +35,12 @@ output "account_id" {
 
 
 locals {
-  secret_access_key_file = "E:/aws/access_key.txt"}
+  secret_access_key_file = "${os.homedir()}/Downloads/access_key.txt"}
 
 resource "local_file" "secret_access_key" {
   content  = join("", [
     "Access Key ID: ", output.access_key_id, "\n",
-    "Secret Access Key: ", output.secret_access_key, "\n",
-    "account_id: ", output.account_id
+    "Secret Access Key: ", output.secret_access_key, "\n"
   ])
   filename = local.secret_access_key_file
 }
