abf11894348c24884f5e605d89ec8eb66814eb19
Chetan-Operisoft <128120269+Chetan-Operisoft@users.noreply.github.com>
2023-03-20 15:36:10 +0530

    Update users.tf

--- users.tf
+++ users.tf
@@ -8,5 +8,5 @@ resource "aws_iam_user_policy_attachment" "ec2-user-full" {
   count      = length(var.username)
   user       = element(aws_iam_user.newusers.*.name, count.index)
   #policy_arn = "${aws_iam_policy.ec2_readonly.arn}"
-  policy_arn = aws_iam_policy.ec2_full.arn
+  #policy_arn = aws_iam_policy.ec2_full.arn
 }
