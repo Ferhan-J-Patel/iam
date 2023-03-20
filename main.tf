dfdb63410d5d5c9772e99768230da721eb766e3f
Chetan-Operisoft <128120269+Chetan-Operisoft@users.noreply.github.com>
2023-03-20 14:20:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ output "account_id" {
 
 
 locals {
-    secret_access_key_file = "E:/aws/access_key.txt"
+    secret_access_key_file = "E:\aws/access_key.txt"
 }
 
 resource "local_file" "secret_access_key" {content  = join("", ("Access Key ID: ",output.access_key_id,"\n","Secret Access Key: ",output.secret_access_key,"\n"))
