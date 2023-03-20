57e7e21e01d6cd930fc1075b54bed7074d9d7d9c
Chetan-Operisoft <128120269+Chetan-Operisoft@users.noreply.github.com>
2023-03-20 18:08:26 +0530

    Update output.tf

--- iam/output.tf
+++ iam/output.tf
@@ -1,9 +1,11 @@
 output "access_key_id" {
   value = aws_iam_access_key.example.id
+  sensitive = true
 }
 
 output "secret_access_key" {
   value = aws_iam_access_key.example.secret
+  sensitive = true
 }
 
 output "account_id" {
