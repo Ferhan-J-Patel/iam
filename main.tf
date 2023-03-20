bf0b152c965e2a7650ce7417367f8593944bf834
Chetan-Operisoft <128120269+Chetan-Operisoft@users.noreply.github.com>
2023-03-20 10:35:02 +0530

    update
    kjl

--- main.tf
+++ main.tf
@@ -6,8 +6,4 @@ resource "aws_iam_user" "newuser" {
     name = "chetan"
 }
 
-resource "aws_iam_policy" "AmazonEC2FullAccess" {
-  name = "AmazonEC2FullAccess"
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
-  user = aws_iam_user.chetan
-}
+
