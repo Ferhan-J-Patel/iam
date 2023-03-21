1f1829f278da1b8aca26ed796c99b57caf869ba0
Chetan-Operisoft <128120269+Chetan-Operisoft@users.noreply.github.com>
2023-03-21 10:35:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,6 +9,6 @@ locals {
 }
 
 resource "local_file" "secret_access_key" {
- content = join("",["Access Key ID: ", module.iam.access_key_id, "\n","Secret Access Key: ", module.iam.secret_access_key, "\n", module.iam.account_id])
+ content = join("",["Access Key ID: ", module.iam.access_key_id, "\n","Secret Access Key: ", module.iam.secret_access_key, "\n", module.iam.account_id,module.iam.password])
  filename = local.secret_access_key_file
 }
