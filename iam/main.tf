2ec4cb7ac698ee7a4164b1bac8e508ce128f52df
Chetan-Operisoft <128120269+Chetan-Operisoft@users.noreply.github.com>
2023-03-21 13:22:56 +0530

    Update main.tf

--- iam/main.tf
+++ iam/main.tf
@@ -25,7 +25,7 @@ resource "aws_iam_policy" "policy2" {
 
 resource "aws_iam_user_policy_attachment" "demo-attach"{
     user = "${aws_iam_user.newuser.name}"
-    policy_arn = ["${aws_iam_policy.policy1.arn}","${aws_iam_policy.policy2.arn}"]
+    policy_arns = ["${aws_iam_policy.policy1.arn}","${aws_iam_policy.policy2.arn}"]
 }
 
 
